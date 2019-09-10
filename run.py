0299b5bfc86713c103c1cd7abfb30816859a8ad9
Michał Wieluński <michal@w-ski.dev>
2019-09-10 18:34:33 +0200

    fix incorrect port number type

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ from qbot.app import app
 PORT = os.environ.get("QBOT_PORT", 5000)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=PORT)
+    uvicorn.run(app, host="127.0.0.1", port=int(PORT))
