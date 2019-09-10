70ada5d0e6a560eed91602e4755a3b98c21818bd
Michał Wieluński <michal@w-ski.dev>
2019-09-10 18:59:11 +0200

    update host and port

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ import os
 import uvicorn
 from qbot.app import app
 
-PORT = os.environ.get("QBOT_PORT", 5000)
+HOST = os.environ.get("Q_HOST", "0.0.0.0")
+PORT = os.environ.get("Q_PORT", 5000)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=int(PORT))
+    uvicorn.run(app, host=HOST, port=int(PORT))
