5c73b6dd3d83d86ea69d43848293619b3c599164
Michał Wieluński <michal@w-ski.dev>
2019-09-20 14:08:56 +0200

    run migrations on CI

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 setup:
 	python3.7 -m venv .venv
 	.venv/bin/python -m pip install -e .[dev]
+	.venv/bin/python -m alembic upgrade head
 	docker run --name pg_qbot --rm -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres:11
+	.venv/bin/python -m alembic downgrade base
 
 destroy:
 	rm -rf .venv
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,15 @@
 import pytest
 from async_asgi_testclient import TestClient
-from sqlalchemy import create_engine
 from starlette.config import environ
 
 from qbot.app import app
 from qbot.core import registry
-from qbot.db import metadata
 
 
 @pytest.fixture(autouse=True, scope="session")
 def configuration():
     environ["TESTING"] = "True"
     registry.set_config_vars()
-
-
-@pytest.fixture(autouse=True, scope="session")
-def create_tables(configuration):
-    engine = create_engine(str(registry.DATABASE_URL))
-    metadata.create_all(engine)
 
 
 @pytest.fixture(autouse=True)
