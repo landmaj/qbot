67070b6e9d36e5dc872a0765763277c953259d54
Michał Wieluński <michal@w-ski.dev>
2019-11-30 17:58:02 +0100

    add total metric

--- qbot/metrics.py
+++ qbot/metrics.py
@@ -4,5 +4,6 @@ from qbot.core import registry
 def incr(stat, count=1, rate=1):
     if hasattr(registry, "statsd"):
         registry.statsd.incr(stat, count, rate)
+        registry.statsd.gauge(f"{stat}", count, delta=True)
     else:
         pass
