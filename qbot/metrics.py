c8a565444cbdd039b0d332120bf26d3734b38a71
Michał Wieluński <michal@w-ski.dev>
2019-12-18 22:38:57 +0100

    remove metrics.py

--- qbot/metrics.py
+++
@@ -1,9 +0,0 @@
-from qbot.core import registry
-
-
-def incr(stat, count=1, rate=1):
-    if hasattr(registry, "statsd"):
-        registry.statsd.incr(stat, count, rate)
-        registry.statsd.gauge(f"{stat}", count, delta=True)
-    else:
-        pass
