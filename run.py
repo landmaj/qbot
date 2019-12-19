5b344f980ac39baad69ae49e2d44a8ae3544197c
Michał Wieluński <michal@w-ski.dev>
2019-12-19 18:18:55 +0100

    add missing function call

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def setup_logging():
 
     sentry_dsn = os.environ.get("Q_SENTRY_DSN")
     if sentry_dsn:
-        sentry_sdk.init(dsn=str(sentry_dsn), release=revision)
+        sentry_sdk.init(dsn=sentry_dsn, release=revision)
 
     loki_url = os.environ.get("LOKI_URL", "localhost:3100")
     loki_user = os.environ.get("LOKI_USER", "")
@@ -33,7 +33,10 @@ def setup_logging():
         level=logging.INFO,
         processors=(add_app_details,),
     )
+    logging.info("Logging setup finished.")
 
+
+setup_logging()
 
 HOST = os.environ.get("Q_HOST", "0.0.0.0")
 PORT = os.environ.get("Q_PORT", 5000)
