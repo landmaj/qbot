19246250b2eddec90373e89936639fc397ddcd9b
Michał Wieluński <michal@w-ski.dev>
2020-02-13 22:49:48 +0100

    changes to parrot plugin

--- qbot/slack/plugin_parrot.py
+++ qbot/slack/plugin_parrot.py
@@ -1,8 +1,10 @@
 from qbot.slack.command import add_command
-from qbot.slack.message import IncomingMessage, delete_message, send_reply
+from qbot.slack.message import IncomingMessage, send_reply
 
 
 @add_command("papuga", "Who the fuck said that!?", aliases=["parrot", "p"])
 async def excuse_command(message: IncomingMessage) -> None:
-    await delete_message(message)
-    await send_reply(message, text=message.text)
+    if message.text:
+        await send_reply(message, text=message.text)
+    else:
+        await send_reply(message, text="Mówiłeś coś?")
