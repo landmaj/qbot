53788c7653fff37bfdc74a484fc7ed282fbdb16d
Michał Wieluński <michal@w-ski.dev>
2020-02-14 00:05:36 +0100

    another round of changes to parrot plugin

--- qbot/slack/plugin_parrot.py
+++ qbot/slack/plugin_parrot.py
@@ -1,38 +1,35 @@
 import logging
+import re
 
 from qbot.slack.command import add_command
 from qbot.slack.message import (
     IncomingMessage,
     OutgoingMessage,
     Text,
-    get_channel_id,
     send_message,
     send_reply,
 )
 
 logger = logging.getLogger(__name__)
+CHANNEL_REGEX = re.compile(r"^<#(?P<channel_id>\w*)\|(?P<channel_name>\w*)>\s")
 
 
-@add_command("papuga", "Who the fuck said that!?", aliases=["parrot", "p"])
-async def excuse_command(message: IncomingMessage) -> None:
+@add_command(
+    "papuga", "Who the fuck said that!?", aliases=["parrot", "p"], safe_to_fix=False
+)
+async def parrot_command(message: IncomingMessage) -> None:
     if message.text:
-        splitted_message = message.text.split(" ", 1)
-        logger.error(message.text)
-        if not splitted_message[0].startswith("#"):
-            await send_reply(message, text="Nazwa kanału powinna zaczynać się `#`.")
-            return
-        elif len(splitted_message) != 2:
+        match = CHANNEL_REGEX.search(message.text)
+        if match is None:
             await send_reply(
-                message, text="SyntaxError: spróbuj !papuga -- #channel message"
+                message, text="SyntaxError: spróbuj `!p -- #fortunki wiadomość"
             )
-            return
-        channel_id = await get_channel_id(splitted_message[0].lstrip("#").rstrip())
-        if channel_id is None:
-            await send_reply(message, text="Na pewno poprawna nazwa kanału?")
             return
         await send_message(
             OutgoingMessage(
-                channel=channel_id, thread_ts=None, blocks=[Text(splitted_message[1])]
+                channel=match.group("channel_id"),
+                thread_ts=None,
+                blocks=[Text(message.text[match.end() :])],
             )
         )
         await send_reply(message, text=message.text)
