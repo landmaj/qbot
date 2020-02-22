3b760827fa536c327cdf8d5dd275c0573c0dc16b
Michał Wieluński <michal@w-ski.dev>
2020-02-22 19:47:54 +0100

    add monkeyuser plugin

--- qbot/plugins/monkeyuser.py
+++ qbot/plugins/monkeyuser.py
@@ -1,18 +1,21 @@
 import logging
 
+import feedparser
+from requests_html import HTML
+
 from qbot.core import registry
 from qbot.db import plugin_storage
 from qbot.message import Image, OutgoingMessage, Text, send_message
 from qbot.scheduler import job
 
-PLUGIN_NAME = "xkcd"
+PLUGIN_NAME = "monkey_user"
 LATEST_COMIC_KEY = "latest_comic"
 
 logger = logging.getLogger(__name__)
 
 
 @job(3600)
-async def xkcd():
+async def monkeyuser():
     last_seen_comic = await registry.database.fetch_val(
         plugin_storage.select().where(
             (plugin_storage.c.plugin == PLUGIN_NAME)
@@ -21,22 +24,25 @@ async def xkcd():
         column=plugin_storage.c.data,
     )
     try:
-        resp = await registry.http_client.get("https://xkcd.com/info.0.json")
+        resp = await registry.http_client.get("https://www.monkeyuser.com/feed.xml")
     except Exception:
-        logger.exception("Failed to retrieve latest XKCD comic.")
+        logger.exception("Failed to retrieve latest monkeyuser comic.")
         return
     if not 200 <= resp.status_code < 400:
-        logger.error(f"Incorrect response from XKCD. Status: {resp.status_code}")
+        logger.error(f"Incorrect response from monkeyuser. Status: {resp.status_code}")
         return
-    data = resp.json()
-    if data["num"] != last_seen_comic:
+    rss = feedparser.parse(resp.text)
+    summary = rss.entries[0]["summary"]
+    parsed_html = HTML(html=summary)
+    data = parsed_html.find("img", first=True).attrs
+    if data["alt"] != last_seen_comic:
         await send_message(
             OutgoingMessage(
                 channel=registry.SPAM_CHANNEL_ID,
                 thread_ts=None,
                 blocks=[
-                    Text(data["safe_title"]),
-                    Image(image_url=data["img"], alt_text=data["alt"]),
+                    Text(data["alt"]),
+                    Image(image_url=data["src"], alt_text=data["title"]),
                 ],
             )
         )
@@ -46,7 +52,7 @@ async def xkcd():
                 values={
                     "plugin": PLUGIN_NAME,
                     "key": LATEST_COMIC_KEY,
-                    "data": data["num"],
+                    "data": data["alt"],
                 },
             )
         else:
@@ -55,5 +61,5 @@ async def xkcd():
                     (plugin_storage.c.plugin == PLUGIN_NAME)
                     & (plugin_storage.c.key == LATEST_COMIC_KEY)
                 ),
-                values={"data": data["num"]},
+                values={"data": data["alt"]},
             )
